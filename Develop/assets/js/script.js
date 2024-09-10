48b075d96e3fb3c889e1452bd117a8ad56c6028d
wilsacker <williamsuttona@gmail.com>
2024-09-09 17:10:07 -0700

    added dayjs

--- Develop/assets/js/script.js
+++ Develop/assets/js/script.js
@@ -4,8 +4,7 @@ let nextId = JSON.parse(localStorage.getItem("nextId"));
 
 // Todo: create a function to generate a unique task id
 function generateTaskId() {
-
-}
+  }
 
 // Todo: create a function to create a task card
 function createTaskCard(task) {
